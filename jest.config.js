59bd2dca9997d881c1f8ab98a16b755608d72598
Denys Vuika <denys.vuika@gmail.com>
2020-06-15 11:58:27 +0100

    coverage setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transformIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  preset: 'ts-jest',
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
 };
