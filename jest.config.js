82e2e664ea222dc595918db8f541a4f03978f7d0
Denys Vuika <denys.vuika@gmail.com>
2020-06-15 12:18:09 +0100

    integrate test library

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,13 @@ module.exports = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageReporters: ['text'],
 };
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { ExampleComponent } from '.';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy();
+  it('renders with custom text', () => {
+    const { getByText } = render(<ExampleComponent text="component works" />);
+    expect(getByText('Example Component: component works')).toBeInTheDocument();
   });
 });
