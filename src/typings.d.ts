928546b63ad61501ec6aa436e71a3e26b8f12ef1
Denys Vuika <denys.vuika@gmail.com>
2020-06-15 09:50:58 +0100

    eslint setup

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -7,11 +7,12 @@ declare module '*.css' {
   export default content;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 
 declare module '*.svg' {
   const svgUrl: string;
   const svgComponent: SvgrComponent;
   export default svgUrl;
-  export { svgComponent as ReactComponent }
+  export { svgComponent as ReactComponent };
 }
