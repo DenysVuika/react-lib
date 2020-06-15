84b129459cd1134a5f606418ea9babbeda8e0616
Denys Vuika <denys.vuika@gmail.com>
2020-06-15 09:35:37 +0100

    project structure and build script

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import copy from 'rollup-plugin-copy';
+// import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import pkg from './package.json';
+import * as path from 'path';
+
+export default [
+  {
+    input: 'src/index.tsx',
+    external: ['react'],
+    plugins: [
+      typescript({
+        typescript: require('typescript'),
+      }),
+      postcss({
+        extract: true,
+        modules: true,
+        plugins: [],
+        extract: path.resolve(`dist/${pkg.name}.css`),
+      }),
+      copy({
+        targets: [{ src: 'package.json', dest: 'dist' }],
+      }),
+      // terser(),
+    ],
+    output: [
+      {
+        name: pkg.name,
+        file: `dist/${pkg.browser}`,
+        format: 'umd',
+        globals: {
+          react: 'react',
+        },
+      },
+      { file: `dist/${pkg.main}`, format: 'cjs' },
+      { file: `dist/${pkg.module}`, format: 'es' },
+    ],
+  },
+];
